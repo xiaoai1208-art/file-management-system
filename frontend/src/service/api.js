ac649fd6963ad7247f40bf1eac077c1683939dc0
xiaoai1208-art <xiaoai1208@gmail.com>
2026-07-23 19:40:36 +0800

    connect frontend to railway backend

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -1,24 +1,20 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api'
+  baseURL: 'https://file-management-system-production.up.railway.app/api'
 })
-
 
 export const getNotes = () => {
   return api.get('/notes')
 }
 
-
 export const createNote = (note) => {
   return api.post('/notes', note)
 }
 
-
 export const updateNote = (id, note) => {
   return api.put(`/notes/${id}`, note)
 }
-
 
 export const deleteNote = (id) => {
   return api.delete(`/notes/${id}`)
